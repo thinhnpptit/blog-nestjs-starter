a91cbcad7e1d467b207041504284231482c08606
thinhnp <thinhnpptit@gmail.com>
2023-10-11 14:25:05 +0700

    config env suggestion

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+const PORT = process.env.PORT || 3000;
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000, () => {
-    console.log(`Server is running at http://localhost:3000/`);
+  await app.listen(PORT, () => {
+    console.log(`Server is running at http://localhost:${PORT}`);
   });
 }
 bootstrap();
