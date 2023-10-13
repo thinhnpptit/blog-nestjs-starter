e0559fdfd056a8ce8492295070a343fc81c2c273
thinhnp <thinhnpptit@gmail.com>
2023-10-13 09:38:00 +0700

    error handling - validation

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import cookieParser from 'cookie-parser';
+import { ValidationPipe } from '@nestjs/common';
 
 const PORT = process.env.PORT || 3000;
 
@@ -11,6 +12,9 @@ async function bootstrap() {
 
   // Apply middlewares
   app.use(cookieParser());
+
+  // Error handling
+  app.useGlobalPipes(new ValidationPipe());
 
   await app.listen(PORT, () => {
     console.log(`Server is running at http://localhost:${PORT}`);
--- src/modules/user/user.entity.ts
+++ src/modules/user/user.entity.ts
@@ -1,5 +1,7 @@
-//user.entity.ts
+// user.entity.ts
 import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+import { IsDate, IsEmail, Min } from 'class-validator';
+import moment from 'moment';
 
 @Entity()
 export class User {
@@ -10,8 +12,26 @@ export class User {
   name: string;
 
   @Column({ unique: true })
+  @IsEmail()
   email: string;
 
   @Column()
+  @Min(0)
   password: string;
+
+  @Column({
+    type: Date,
+    default: moment(new Date()).format('YYYY-MM-DD HH:ss'),
+    nullable: true,
+  })
+  @IsDate()
+  createdAt: Date;
+
+  @Column({
+    type: Date,
+    default: moment(new Date()).format('YYYY-MM-DD HH:ss'),
+    nullable: true,
+  })
+  @IsDate()
+  updatedAt: Date;
 }
